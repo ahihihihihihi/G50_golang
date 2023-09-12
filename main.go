128a3b1626089ba3243d018ae9b82cd4f5576e83
ahihi <bdsthocu20@gmail.com>
2023-09-12 15:08:55 +0700

    #3.2

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
 )
 
 type Restaurant struct {
@@ -28,6 +31,7 @@ func (RestaurantUpdate) TableName() string {
 
 //go get gorm.io/gorm@v1.20.11
 //go get gorm.io/driver/mysql@v1.0.3
+//go get -u github.com/gin-gonic/gin@v1.7.1
 
 func main() {
 
@@ -40,26 +44,189 @@ func main() {
 		log.Fatalln(err)
 	}
 
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	v1 := r.Group("/v1")
+
+	restaurant := v1.Group("/restaurants")
+
+	restaurant.POST("", func(c *gin.Context) {
+		var data Restaurant
+
+		if err := c.ShouldBindJSON(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"err": err.Error(),
+			})
+
+			return
+		}
+
+		if err := db.Create(&data).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": err.Error(),
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": data,
+		})
+	})
+
+	restaurant.GET("/:id", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"err": err.Error(),
+			})
+
+			return
+		}
+
+		var data Restaurant
+
+		if err := db.Where("id = ?", id).First(&data).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": err.Error(),
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": data,
+		})
+	})
+
+	restaurant.GET("", func(c *gin.Context) {
+
+		var data []Restaurant
+
+		type Paging struct {
+			Page int `json:"page" form:"page"`
+			Limit int `json:"limit" form:"limit"`
+		}
+
+		var pagingData Paging
+
+		if err := c.ShouldBind(&pagingData); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"err": err.Error(),
+			})
+
+			return
+		}
+
+		if pagingData.Page <= 0 {
+			pagingData.Page = 1
+		}
+		if pagingData.Limit <= 0 {
+			pagingData.Limit = 5
+		}
+
+		if err := db.
+			Offset((pagingData.Page - 1) * pagingData.Limit).
+			Order("id desc").Limit(pagingData.Limit).
+			Find(&data).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": err.Error(),
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": data,
+		})
+	})
+
+	restaurant.PATCH("/:id", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"err": err.Error(),
+			})
+
+			return
+		}
+
+		var data RestaurantUpdate
+
+		if err := c.ShouldBindJSON(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"err": err.Error(),
+			})
+
+			return
+		}
+
+		if err := db.Where("id = ?", id).Updates(&data).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": err.Error(),
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": true,
+		})
+	})
+
+	restaurant.DELETE("/:id", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"err": err.Error(),
+			})
+
+			return
+		}
+
+		if err := db.Table(Restaurant{}.TableName()).Where("id = ?", id).Delete(nil).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": err.Error(),
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": true,
+		})
+	})
+
+	r.Run()
+
 	//log.Println(db,err)
 
-	newRestaurant := Restaurant{
-		Name: "Tani",
-		Addr: "9 Pham Van Hai",
-	}
-
-	if err := db.Create(&newRestaurant).Error; err != nil {
-		log.Println(err)
-	}
-
-	log.Println("New ID Created: ", newRestaurant.Id)
-
-	var myRestaurant Restaurant
-
-	if err := db.Where("id = ?", 2).First(&myRestaurant).Error; err != nil {
-		log.Println(err)
-	}
-
-	log.Println(myRestaurant)
+	//newRestaurant := Restaurant{
+	//	Name: "Tani",
+	//	Addr: "9 Pham Van Hai",
+	//}
+	//
+	//if err := db.Create(&newRestaurant).Error; err != nil {
+	//	log.Println(err)
+	//}
+	//
+	//log.Println("New ID Created: ", newRestaurant.Id)
+	//
+	//var myRestaurant Restaurant
+	//
+	//if err := db.Where("id = ?", 2).First(&myRestaurant).Error; err != nil {
+	//	log.Println(err)
+	//}
+	//
+	//log.Println(myRestaurant)
 
 	//myRestaurant.Name = "500lab"
 
@@ -69,17 +236,17 @@ func main() {
 	//
 	//log.Println(myRestaurant)
 
-	update := "500lab"
-	updateData := RestaurantUpdate{Name: &update}
-	if err := db.Where("id = ?", 2).Updates(&updateData).Error; err != nil {
-		log.Println(err)
-	}
-
-	log.Println(*updateData.Name)
-
-	if err := db.Table(Restaurant{}.TableName()).Where("id = ?", 5).Delete(nil).Error; err != nil {
-		log.Println(err)
-	}
+	//update := "500lab"
+	//updateData := RestaurantUpdate{Name: &update}
+	//if err := db.Where("id = ?", 2).Updates(&updateData).Error; err != nil {
+	//	log.Println(err)
+	//}
+	//
+	//log.Println(*updateData.Name)
+	//
+	//if err := db.Table(Restaurant{}.TableName()).Where("id = ?", 5).Delete(nil).Error; err != nil {
+	//	log.Println(err)
+	//}
 
 }
 
